90d6565921c1cada2ac31f54c7c22d02a71f15e8
umer999malik <umer999malik@gmail.com>
2026-06-01 17:07:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ closeBtn.addEventListener('click', () => lightbox.classList.remove('active'));
 lightbox.addEventListener('click', (e) => {
   if (e.target === lightbox) lightbox.classList.remove('active');
 });
+document.querySelectorAll(".thumb").forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -8;
+    const rotateY = ((x - centerX) / centerX) * 8;
+
+    card.style.transform =
+      `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform =
+      "rotateX(0deg) rotateY(0deg) scale(1)";
+  });
+});
